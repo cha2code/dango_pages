e088ebb0b4d437f632e37ed4870badf122ae722f
cha2jini <cha2jini@naver.com>
2024-03-21 16:33:35 +0900

    :tada: dango_pages 생성

---
+++ src/main/java/org/cha2code/dango_pages/DangoPagesApplication.java
@@ -0,0 +1,13 @@
+package org.cha2code.dango_pages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangoPagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangoPagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cha2code/dango_pages/DangoPagesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cha2code.dango_pages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangoPagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
