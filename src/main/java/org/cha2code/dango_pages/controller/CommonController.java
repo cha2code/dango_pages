4894671f8ae7f2f6ad09cad7002affe38add54bc
cha2jini <cha2jini@naver.com>
2024-04-02 20:37:43 +0900

    🍱 리소스 수정 및 페이지 생성
    - 회원가입 페이지 생성
    - loginUtil.css -> utils.css로 변경
    - css파일 내 class명 변경 및 추가
    - 수정된 리소스에 따라 로그인 페이지 수정

--- src/main/java/org/cha2code/dango_pages/controller/CommonController.java
+++ src/main/java/org/cha2code/dango_pages/controller/CommonController.java
@@ -2,6 +2,8 @@ package org.cha2code.dango_pages.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CommonController {
@@ -11,8 +13,13 @@ public class CommonController {
 		return "index";
 	}
 
-	@GetMapping("/login")
-	public String userLogin() {
+	@RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
+	public String loginPage() {
 		return "pages/userLogin";
+	}
+
+	@RequestMapping(value = "/signup", method = {RequestMethod.GET, RequestMethod.POST})
+	public String signUp() {
+		return "pages/signUp";
 	}
 }
